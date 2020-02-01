a52fca67869ff0bb428af0650d224f46e9bc651d
Suffren <regis.cleis@hotmail.fr>
2020-02-01 12:20:32 +0100

    - Convertion en fonctions fléchées - Désactiver les boutons de lectures lors de la séquence

--- js/app.js
+++ js/app.js
@@ -2,54 +2,52 @@
 
 const pads = ['green', 'red', 'yellow', 'blue'];
 
-let playSeqBtn = document.getElementsByClassName('btn2')[0];
-playSeqBtn.setAttribute('disabled', 'disabled');
-playSeqBtn.addEventListener('click', function () {
-    playSequence(sequence, 500).then(function () { playSeqBtn.removeAttribute('disabled')});
-});
+let playSeqBtn = document.getElementById('playSeq');
+let btns = [...document.getElementsByClassName('btn')];
 
-let startSequence = [2,3,1,0];
+playSeqBtn.setAttribute('disabled', 'disabled');
+playSeqBtn.addEventListener('click', () =>
+    playSequence(sequence, 500).then( () => enablePlayButtons() )
+);
+
+const startSequence = [2,3,1,0];
 let sequence = [];
-function addRandomSound () {
+let addRandomSound = () => {
     let newSound = Math.floor(Math.random() * Math.floor(4));
     sequence.push(newSound);
 }
 
 let success = 0;
 let startBtn = document.getElementsByClassName('btn')[0];
-startBtn.addEventListener('click', function () { // Réinitialisation
+startBtn.addEventListener('click', () => { // Réinitialisation
     success = 0;
     sequence = [];
     addRandomSound();
-    playSequence(startSequence, 200).then(function () {
-        setTimeout(
-            function () {
-                playSeqBtn.removeAttribute('disabled');
+    playSequence(startSequence, 200).then( () => {
+        setTimeout( () => {
+                btns.forEach( el => el.removeAttribute('disabled'))
                 activatePad(pads[sequence[0]]);
-            },
-        1000)
-        });
+        }, 1000)
+    });
 });
 
-function playPad (padName) {
+let playPad = (padName) => {
     activatePad(padName);
     if(sequence.length !== 0)
         checkSequence(padName);
 }
 
-function activatePad (padName) {
+let activatePad = (padName) => {
     // Jouer le son concerné
     let audio = new Audio(document.getElementById(padName).children[0].src);
     audio.play();
 
-    // Allumer et éteindre le bouton
+    // Allumer et éteindre le pad
     document.getElementById(padName).style = 'background-color: purple; box-shadow: 0px 0px 30px purple';
-    setTimeout(function () {
-        document.getElementById(padName).style = `background-color: ${padName}; box-shadow: 0px`;
-    }, 300);
+    setTimeout( () => document.getElementById(padName).style = `background-color: ${padName}; box-shadow: 0px`, 300);
 }
 
-function checkSequence (padName) {
+let checkSequence = (padName) => {
     let idxActivePad = pads.indexOf(padName);// Trouver l'index du pad concerné
     if(sequence[success] === idxActivePad) { // Vérifier si le pad cliqué correspond à l'élément courant de la séquence
         success += 1;                          // Si oui, c'est un essai réussi de plus
@@ -61,7 +59,7 @@ function checkSequence (padName) {
 
             setTimeout( () => {                // Sinon, refaire écouter la séquence en y ajoutant un nouveau son
                 addRandomSound();
-                playSequence(sequence, 500).then(function() { playSeqBtn.removeAttribute('disabled')});
+                playSequence(sequence, 500).then( () => enablePlayButtons() );
             } , 1000);                         // Léger délai avant la lecture de la séquence
 
             success = 0;
@@ -73,18 +71,20 @@ function checkSequence (padName) {
     }
 }
 
-function playSequence (seq, delay) {
+let playSequence = (seq, delay) => {
     success = 0;
-    playSeqBtn.setAttribute('disabled', 'disabled');    // Désactiver le bouton de lecture pendant celle ci
+    btns.forEach( el => el.setAttribute('disabled', 'disabled')); // Désactiver les boutons "play" lors de la lecture
 
-    var promise = new Promise( resolve => resolve());   // Création d'un promesse déjà tenue
-    seq.forEach(function (el, idx) {                    // Pour chaque élement de la séquence
-        promise = promise.then(function () {                // Excécution asynchrone
-            activatePad(pads[seq[idx]]);                    // ... de l'activation d'un pad
-            return new Promise(function (resolve) {         // puis attendre le temps du délai
-                setTimeout(resolve, delay);
-            });
+    var promise = Promise.resolve();                    // Création d'un promesse déjà tenue
+    seq.forEach( (el, idx) => {                         // Pour chaque élement de la séquence
+        promise = promise.then( () => {                     // Excécution asynchrone
+            activatePad(pads[seq[idx]]);                        // ... de l'activation d'un pad
+            return new Promise( (resolve) =>                    // puis attendre le temps du délai
+                setTimeout(resolve, delay)
+            );
         });
     });
     return promise;
 }
+
+let enablePlayButtons = () => ( btns.forEach( el => el.removeAttribute('disabled')) );
